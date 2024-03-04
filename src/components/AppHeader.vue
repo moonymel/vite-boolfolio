a0ff0a3ea27b629bc47561e649100b31e3e65bd2
moonymel <musumeci.me@gmail.com>
2024-03-04 15:32:41 +0100

    [UPDATE] created AppMain.vue with api projects

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+    <header>
+        <div class="container py-3">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    ciao sono un header che sistemerò più avanti
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+@use '../styles/general.scss';
+
+header {
+    background-color:lightgrey;
+}
+</style>
---
+++ src/components/SingleProject.vue
@@ -0,0 +1,33 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    name: 'SingleProject',
+    props: {
+        project: Object
+    },
+    data(){
+        return {
+            store
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div class="col-3">
+        <div class="card">
+            <img :src="`${store.baseUrl}/storage/${project.preview_image}`" alt="" class="card-img-top">
+            <div class="card-body">
+                <h6>{{ project.title }}</h6>
+            </div>
+            <p>
+                {{ project.description }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@use '../styles/general.scss';
+</style>./SingleProject.vue/index.js
